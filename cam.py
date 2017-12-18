ad768ec06d43a9a97c71d6f4abaf8436a756c880
Mich <lemich@gmail.com>
2017-12-18 04:09:43 +0000

    Debugging

--- cam.py
+++ cam.py
@@ -1,12 +1,16 @@
 import picamera
 import RPi.GPIO as GPIO
 import time
+import boto3
+from subprocess import call
 
 camera = picamera.PiCamera()
 camera.resolution = (1920, 1080)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN) #PIR
+boto3.set_stream_logger('')
+s3 = boto3.resource('s3')
 
 try:
     time.sleep(2) # to stabilize sensor
@@ -14,19 +18,37 @@ try:
         if GPIO.input(23):
             millis = int(round(time.time() * 1000))
             print("Motion detected, taking photo...")
-	    camera.resolution = (1920, 1080)
-            camera.capture('capture-{}-pic.jpg'.format(millis))
+	    picname = 'capture-{}-pic.jpg'.format(millis) 
+            videoname = 'capture-{}-video.h264'.format(millis)
+            camera.resolution = (1920, 1080)
+            camera.capture(picname)
             print("Done taking photo, making video")
 	    camera.resolution = (1280, 720)
-            camera.start_recording('capture-{}-video.h264'.format(millis))
-            while GPIO.input(23): #do not stop recording while stuff is moving
-		camera.wait_recording(15)
-                print("Extending...")
+            camera.start_recording(videoname)
+	    camera.wait_recording(45)
+            while GPIO.input(23):
+		camera.wait_recording(10)
+                print("There still is motion. Extending...")
 
             camera.stop_recording()
-	    print("Done with video. Resuming motion detection...")
-            time.sleep(5) #to avoid multiple detection
-        time.sleep(0.1) #loop delay, should be less than detection delay
+	    print("Done recording")
+            try:
+	           print("Uploading video to S3...")
+		   mp4videoname = 'capture-{}-video.mp4'.format(millis)
+ 	   	   call(["ffmpeg","-framerate","24","-i",videoname,"-c","copy",mp4videoname])
+		   call(["rm",videoname])
+		   data = open(mp4videoname, 'rb')
+		   s3.Bucket('zuzuthecat').put_object(Key=mp4videoname, Body=data)
+		   print("Done")
+	 	   print("Uploading photo to S3...")
+	           data = open(picname, 'rb')
+            	   s3.Bucket('zuzuthecat').put_object(Key=picname, Body=data)
+            	   print("Done with video. Resuming motion detection...")
+
+            except:
+		   print("Failed to upload videos. Check wifi!")
+
+	time.sleep(0.1) #loop delay, should be less than detection delay
 
 except:
     GPIO.cleanup()
