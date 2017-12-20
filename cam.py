297e2eee2dbc8ec7b2b06816088b8e0e1936cabf
Mich <lemich@gmail.com>
2017-12-20 06:51:09 +0000

    Path for storage can be passed in

--- cam.py
+++ cam.py
@@ -3,6 +3,9 @@ import RPi.GPIO as GPIO
 import time
 import boto3
 from subprocess import call
+from os.path import join
+import sys
+import os
 
 camera = picamera.PiCamera()
 camera.resolution = (1920, 1080)
@@ -12,7 +15,9 @@ GPIO.setup(23, GPIO.IN) #PIR
 boto3.set_stream_logger('')
 s3 = boto3.resource('s3')
 
-print('Starting camera...')
+mediapath = sys.argv[1] if len(sys.argv)>1 else os.path.dirname(os.path.realpath(__file__))
+
+print('Starting camera, media path is {}'.format(mediapath))
 
 try:
     time.sleep(2) # to stabilize sensor
@@ -22,11 +27,13 @@ try:
             print("Motion detected, taking photo...")
 	    picname = 'capture-{}-pic.jpg'.format(millis) 
             videoname = 'capture-{}-video.h264'.format(millis)
+            fullpicname = join(mediapath,picname)
+	    fullvideoname = join(mediapath,videoname)
             camera.resolution = (1920, 1080)
-            camera.capture(picname)
+            camera.capture(fullpicname)
             print("Done taking photo, making video")
 	    camera.resolution = (1280, 720)
-            camera.start_recording(videoname)
+            camera.start_recording(fullvideoname)
 	    camera.wait_recording(45)
             while GPIO.input(23):
 		camera.wait_recording(10)
@@ -37,8 +44,8 @@ try:
             try:
 	           print("Converting h264 to mp4...")
 		   mp4videoname = 'capture-{}-video.mp4'.format(millis)
- 	   	   call(["ffmpeg","-framerate","24","-i",videoname,"-c","copy",mp4videoname])
-		   call(["rm",videoname])
+ 	   	   call(["ffmpeg","-framerate","24","-i",fullvideoname,"-c","copy",join(mediapath,mp4videoname)])
+		   call(["rm",fullvideoname])
 		   print("Done. Resuming motion detection.")
 
             except:
